1f9bda68731d1ac5ec0c6ac4f8c7a8f0211e2480
Sakshi Devkar <2001sakshidev@gmail.com>
2023-06-30 09:54:30 +0530

    C# Assignment

---
+++ Assignment_05/Control_Assignment/Control_Assignment/Windows_Tools.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Control_Assignment
+{
+    partial class frm_Windows_Tools
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Employee_Details = new System.Windows.Forms.Label();
+            this.lbl_Employee_Name = new System.Windows.Forms.Label();
+            this.lbl_Employee_Department = new System.Windows.Forms.Label();
+            this.lbl_Gender = new System.Windows.Forms.Label();
+            this.lbl_Shift_Time = new System.Windows.Forms.Label();
+            this.lbl_Output = new System.Windows.Forms.Label();
+            this.tb_Employee_Name = new System.Windows.Forms.TextBox();
+            this.tb_Output = new System.Windows.Forms.TextBox();
+            this.cmb_Employee_Department = new System.Windows.Forms.ComboBox();
+            this.gb_Gender = new System.Windows.Forms.GroupBox();
+            this.gb_Shift_Time = new System.Windows.Forms.GroupBox();
+            this.rb_Male = new System.Windows.Forms.RadioButton();
+            this.rb_Female = new System.Windows.Forms.RadioButton();
+            this.rb_Night = new System.Windows.Forms.RadioButton();
+            this.rb_Morning = new System.Windows.Forms.RadioButton();
+            this.rb_Evening = new System.Windows.Forms.RadioButton();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.gb_Gender.SuspendLayout();
+            this.gb_Shift_Time.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_Employee_Details
+            // 
+            this.lbl_Employee_Details.AutoSize = true;
+            this.lbl_Employee_Details.BackColor = System.Drawing.Color.PeachPuff;
+            this.lbl_Employee_Details.Font = new System.Drawing.Font("Modern No. 20", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Employee_Details.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.lbl_Employee_Details.Location = new System.Drawing.Point(280, 21);
+            this.lbl_Employee_Details.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Employee_Details.Name = "lbl_Employee_Details";
+            this.lbl_Employee_Details.Size = new System.Drawing.Size(302, 41);
+            this.lbl_Employee_Details.TabIndex = 0;
+            this.lbl_Employee_Details.Text = "Employee Details";
+            // 
+            // lbl_Employee_Name
+            // 
+            this.lbl_Employee_Name.AutoSize = true;
+            this.lbl_Employee_Name.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Employee_Name.ForeColor = System.Drawing.Color.Purple;
+            this.lbl_Employee_Name.Location = new System.Drawing.Point(80, 114);
+            this.lbl_Employee_Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Employee_Name.Name = "lbl_Employee_Name";
+            this.lbl_Employee_Name.Size = new System.Drawing.Size(165, 25);
+            this.lbl_Employee_Name.TabIndex = 1;
+            this.lbl_Employee_Name.Text = "Employee Name";
+            // 
+            // lbl_Employee_Department
+            // 
+            this.lbl_Employee_Department.AutoSize = true;
+            this.lbl_Employee_Department.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Employee_Department.ForeColor = System.Drawing.Color.Purple;
+            this.lbl_Employee_Department.Location = new System.Drawing.Point(80, 191);
+            this.lbl_Employee_Department.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Employee_Department.Name = "lbl_Employee_Department";
+            this.lbl_Employee_Department.Size = new System.Drawing.Size(222, 25);
+            this.lbl_Employee_Department.TabIndex = 1;
+            this.lbl_Employee_Department.Text = "Employee Department";
+            // 
+            // lbl_Gender
+            // 
+            this.lbl_Gender.AutoSize = true;
+            this.lbl_Gender.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Gender.ForeColor = System.Drawing.Color.Purple;
+            this.lbl_Gender.Location = new System.Drawing.Point(80, 267);
+            this.lbl_Gender.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Gender.Name = "lbl_Gender";
+            this.lbl_Gender.Size = new System.Drawing.Size(80, 25);
+            this.lbl_Gender.TabIndex = 1;
+            this.lbl_Gender.Text = "Gender";
+            // 
+            // lbl_Shift_Time
+            // 
+            this.lbl_Shift_Time.AutoSize = true;
+            this.lbl_Shift_Time.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Shift_Time.ForeColor = System.Drawing.Color.Purple;
+            this.lbl_Shift_Time.Location = new System.Drawing.Point(80, 344);
+            this.lbl_Shift_Time.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Shift_Time.Name = "lbl_Shift_Time";
+            this.lbl_Shift_Time.Size = new System.Drawing.Size(109, 25);
+            this.lbl_Shift_Time.TabIndex = 1;
+            this.lbl_Shift_Time.Text = "Shift Time";
+            // 
+            // lbl_Output
+            // 
+            this.lbl_Output.AutoSize = true;
+            this.lbl_Output.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Output.ForeColor = System.Drawing.Color.Purple;
+            this.lbl_Output.Location = new System.Drawing.Point(82, 517);
+            this.lbl_Output.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Output.Name = "lbl_Output";
+            this.lbl_Output.Size = new System.Drawing.Size(78, 25);
+            this.lbl_Output.TabIndex = 1;
+            this.lbl_Output.Text = "Output";
+            // 
+            // tb_Employee_Name
+            // 
+            this.tb_Employee_Name.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Employee_Name.Location = new System.Drawing.Point(413, 116);
+            this.tb_Employee_Name.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_Employee_Name.Name = "tb_Employee_Name";
+            this.tb_Employee_Name.Size = new System.Drawing.Size(283, 26);
+            this.tb_Employee_Name.TabIndex = 1;
+            this.tb_Employee_Name.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Only_Text);
+            // 
+            // tb_Output
+            // 
+            this.tb_Output.Font = new System.Drawing.Font("Georgia", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Output.Location = new System.Drawing.Point(189, 519);
+            this.tb_Output.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_Output.Name = "tb_Output";
+            this.tb_Output.Size = new System.Drawing.Size(637, 25);
+            this.tb_Output.TabIndex = 2;
+            // 
+            // cmb_Employee_Department
+            // 
+            this.cmb_Employee_Department.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Employee_Department.FormattingEnabled = true;
+            this.cmb_Employee_Department.Items.AddRange(new object[] {
+            "R&D",
+            "HRD",
+            "Testing",
+            "Management"});
+            this.cmb_Employee_Department.Location = new System.Drawing.Point(413, 193);
+            this.cmb_Employee_Department.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmb_Employee_Department.Name = "cmb_Employee_Department";
+            this.cmb_Employee_Department.Size = new System.Drawing.Size(277, 26);
+            this.cmb_Employee_Department.TabIndex = 2;
+            // 
+            // gb_Gender
+            // 
+            this.gb_Gender.BackColor = System.Drawing.Color.LavenderBlush;
+            this.gb_Gender.Controls.Add(this.rb_Female);
+            this.gb_Gender.Controls.Add(this.rb_Male);
+            this.gb_Gender.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_Gender.Location = new System.Drawing.Point(413, 258);
+            this.gb_Gender.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gb_Gender.Name = "gb_Gender";
+            this.gb_Gender.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gb_Gender.Size = new System.Drawing.Size(384, 53);
+            this.gb_Gender.TabIndex = 3;
+            this.gb_Gender.TabStop = false;
+            this.gb_Gender.Text = "Gender";
+            // 
+            // gb_Shift_Time
+            // 
+            this.gb_Shift_Time.BackColor = System.Drawing.Color.LavenderBlush;
+            this.gb_Shift_Time.Controls.Add(this.rb_Night);
+            this.gb_Shift_Time.Controls.Add(this.rb_Evening);
+            this.gb_Shift_Time.Controls.Add(this.rb_Morning);
+            this.gb_Shift_Time.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_Shift_Time.Location = new System.Drawing.Point(413, 357);
+            this.gb_Shift_Time.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gb_Shift_Time.Name = "gb_Shift_Time";
+            this.gb_Shift_Time.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gb_Shift_Time.Size = new System.Drawing.Size(384, 54);
+            this.gb_Shift_Time.TabIndex = 4;
+            this.gb_Shift_Time.TabStop = false;
+            this.gb_Shift_Time.Text = "Shift Time";
+            // 
+            // rb_Male
+            // 
+            this.rb_Male.AutoSize = true;
+            this.rb_Male.Location = new System.Drawing.Point(18, 26);
+            this.rb_Male.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rb_Male.Name = "rb_Male";
+            this.rb_Male.Size = new System.Drawing.Size(56, 20);
+            this.rb_Male.TabIndex = 0;
+            this.rb_Male.TabStop = true;
+            this.rb_Male.Text = "Male";
+            this.rb_Male.UseVisualStyleBackColor = true;
+            // 
+            // rb_Female
+            // 
+            this.rb_Female.AutoSize = true;
+            this.rb_Female.Location = new System.Drawing.Point(224, 26);
+            this.rb_Female.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rb_Female.Name = "rb_Female";
+            this.rb_Female.Size = new System.Drawing.Size(71, 20);
+            this.rb_Female.TabIndex = 0;
+            this.rb_Female.TabStop = true;
+            this.rb_Female.Text = "Female";
+            this.rb_Female.UseVisualStyleBackColor = true;
+            // 
+            // rb_Night
+            // 
+            this.rb_Night.AutoSize = true;
+            this.rb_Night.Location = new System.Drawing.Point(312, 26);
+            this.rb_Night.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rb_Night.Name = "rb_Night";
+            this.rb_Night.Size = new System.Drawing.Size(60, 20);
+            this.rb_Night.TabIndex = 0;
+            this.rb_Night.TabStop = true;
+            this.rb_Night.Text = "Night";
+            this.rb_Night.UseVisualStyleBackColor = true;
+            // 
+            // rb_Morning
+            // 
+            this.rb_Morning.AutoSize = true;
+            this.rb_Morning.Location = new System.Drawing.Point(18, 26);
+            this.rb_Morning.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rb_Morning.Name = "rb_Morning";
+            this.rb_Morning.Size = new System.Drawing.Size(79, 20);
+            this.rb_Morning.TabIndex = 0;
+            this.rb_Morning.TabStop = true;
+            this.rb_Morning.Text = "Morning";
+            this.rb_Morning.UseVisualStyleBackColor = true;
+            // 
+            // rb_Evening
+            // 
+            this.rb_Evening.AutoSize = true;
+            this.rb_Evening.Location = new System.Drawing.Point(176, 26);
+            this.rb_Evening.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rb_Evening.Name = "rb_Evening";
+            this.rb_Evening.Size = new System.Drawing.Size(76, 20);
+            this.rb_Evening.TabIndex = 0;
+            this.rb_Evening.TabStop = true;
+            this.rb_Evening.Text = "Evening";
+            this.rb_Evening.UseVisualStyleBackColor = true;
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.BackColor = System.Drawing.Color.MediumPurple;
+            this.btn_Reset.Font = new System.Drawing.Font("Georgia", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Reset.ForeColor = System.Drawing.Color.HotPink;
+            this.btn_Reset.Location = new System.Drawing.Point(589, 446);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(109, 44);
+            this.btn_Reset.TabIndex = 6;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = false;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.BackColor = System.Drawing.Color.MediumPurple;
+            this.btn_Submit.Font = new System.Drawing.Font("Georgia", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.ForeColor = System.Drawing.Color.HotPink;
+            this.btn_Submit.Location = new System.Drawing.Point(275, 446);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(109, 44);
+            this.btn_Submit.TabIndex = 5;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = false;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // frm_Windows_Tools
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 559);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.cmb_Employee_Department);
+            this.Controls.Add(this.gb_Shift_Time);
+            this.Controls.Add(this.gb_Gender);
+            this.Controls.Add(this.tb_Output);
+            this.Controls.Add(this.tb_Employee_Name);
+            this.Controls.Add(this.lbl_Output);
+            this.Controls.Add(this.lbl_Shift_Time);
+            this.Controls.Add(this.lbl_Gender);
+            this.Controls.Add(this.lbl_Employee_Department);
+            this.Controls.Add(this.lbl_Employee_Name);
+            this.Controls.Add(this.lbl_Employee_Details);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Windows_Tools";
+            this.Text = "Windows Tools";
+            this.Load += new System.EventHandler(this.frm_Windows_Tools_Load);
+            this.gb_Gender.ResumeLayout(false);
+            this.gb_Gender.PerformLayout();
+            this.gb_Shift_Time.ResumeLayout(false);
+            this.gb_Shift_Time.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Employee_Details;
+        private System.Windows.Forms.Label lbl_Employee_Name;
+        private System.Windows.Forms.Label lbl_Employee_Department;
+        private System.Windows.Forms.Label lbl_Gender;
+        private System.Windows.Forms.Label lbl_Shift_Time;
+        private System.Windows.Forms.Label lbl_Output;
+        private System.Windows.Forms.TextBox tb_Employee_Name;
+        private System.Windows.Forms.TextBox tb_Output;
+        private System.Windows.Forms.ComboBox cmb_Employee_Department;
+        private System.Windows.Forms.GroupBox gb_Gender;
+        private System.Windows.Forms.GroupBox gb_Shift_Time;
+        private System.Windows.Forms.RadioButton rb_Female;
+        private System.Windows.Forms.RadioButton rb_Male;
+        private System.Windows.Forms.RadioButton rb_Night;
+        private System.Windows.Forms.RadioButton rb_Evening;
+        private System.Windows.Forms.RadioButton rb_Morning;
+        private System.Windows.Forms.Button btn_Reset;
+        private System.Windows.Forms.Button btn_Submit;
+    }
+}
+
